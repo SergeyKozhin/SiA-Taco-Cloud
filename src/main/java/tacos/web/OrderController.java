04aeb702fa7cd2754e420d1beddda998e05f1534
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-04-12 02:54:21 +0300

    Added simple order processsing

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.Order;
 
 @Slf4j
 @Controller
@@ -14,7 +16,13 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm(Model model) {
         model.addAttribute("order", new Order());
-        return "orederForm";
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order) {
+        log.info("Order submitted: " + order);
+        return "redirect:/";
     }
 
 }
