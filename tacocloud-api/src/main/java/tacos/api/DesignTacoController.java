4e6949854c9e855c8bb2a7367f5f2cc3603afe8c
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-04-15 17:16:31 +0300

    Added rest controllers for orders and tacos

---
+++ tacocloud-api/src/main/java/tacos/api/DesignTacoController.java
@@ -0,0 +1,57 @@
+package tacos.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import tacos.Order;
+import tacos.Taco;
+import tacos.data.TacoRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/design",
+        produces = "application/json")
+@CrossOrigin(origins = "*")
+public class DesignTacoController {
+    private final TacoRepository tacoRepo;
+    private final EntityLinks entityLinks;
+
+    public DesignTacoController(TacoRepository tacoRepo, EntityLinks entityLinks) {
+        this.tacoRepo = tacoRepo;
+        this.entityLinks = entityLinks;
+    }
+
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        return tacoRepo.findAll(page).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optTaco = tacoRepo.findById(id);
+        return optTaco.map(taco ->
+                new ResponseEntity<>(taco, HttpStatus.OK)).orElseGet(() ->
+                new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
+}
