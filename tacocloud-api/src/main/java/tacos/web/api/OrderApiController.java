8e708356ee0e567d2ce87151b81d33b752fd7215
Sergey Kozhin <kozhinsergeyv@gmail.com>
2020-06-18 02:27:56 +0300

    Implemented messaging with artemis

--- tacocloud-api/src/main/java/tacos/web/api/OrderApiController.java
+++ tacocloud-api/src/main/java/tacos/web/api/OrderApiController.java
@@ -4,6 +4,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import tacos.Order;
 import tacos.data.OrderRepository;
+import tacos.messaging.OrderMessagingService;
 
 @RestController
 @RequestMapping(path = "/orders",
@@ -21,14 +23,22 @@ import tacos.data.OrderRepository;
 public class OrderApiController {
 
     private final OrderRepository orderRepo;
+    private final OrderMessagingService messagingService;
 
-    public OrderApiController(OrderRepository orderRepo) {
+    public OrderApiController(OrderRepository orderRepo, OrderMessagingService messagingService) {
         this.orderRepo = orderRepo;
+        this.messagingService = messagingService;
+    }
+
+    @GetMapping
+    public Iterable<Order> getAllOrders() {
+        return orderRepo.findAll();
     }
 
     @PostMapping(consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public Order createOrder(@RequestBody Order order) {
+        messagingService.sendOrder(order);
         return orderRepo.save(order);
     }
 
---
+++ tacocloud-kitchen/src/main/java/tacos/kitchen/messaging/jms/JmsOrderReceiver.java
@@ -0,0 +1,22 @@
+package tacos.kitchen.messaging.jms;
+
+import tacos.Order;
+import tacos.kitchen.OrderReceiver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JmsOrderReceiver implements OrderReceiver {
+    private final JmsTemplate jms;
+
+    @Autowired
+    public JmsOrderReceiver(JmsTemplate jms) {
+        this.jms = jms;
+    }
+
+    @Override
+    public Order receiveOrder() {
+        return (Order) jms.receiveAndConvert("tacocloud.order.queue");
+    }
+}
